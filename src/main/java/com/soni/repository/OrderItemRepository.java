1cc11a6a9ae2885aebe51ad0a06bcedc0aa74b73
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 20:57:20 +0530

    Added OrderItemRepository,OrderRepository,PasswordResetTokenRepository

--- src/main/java/com/soni/repository/OrderItemRepository.java
+++ src/main/java/com/soni/repository/OrderItemRepository.java
@@ -1,9 +1,9 @@
-//package com.soni.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository; 
-//
-//import com.soni.model.OrderItem;
-//
-//public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-//
-//}
+package com.soni.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+import com.soni.model.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
--- src/main/java/com/soni/repository/OrderRepository.java
+++ src/main/java/com/soni/repository/OrderRepository.java
@@ -1,19 +1,19 @@
-//package com.soni.repository;
-//
-//import java.util.List; 
-//import java.util.Optional;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//
-//import com.soni.model.Order;
-//import com.soni.model.User;
-//
-//public interface OrderRepository extends JpaRepository<Order,Long> {
-//	@Query("SELECT o FROM Order o WHERE o.customer.id = :userId")
-//	Optional<List<Order>> findAllUserOrders(@Param("userId")Long userId);
-//    
-//	@Query("SELECT o FROM Order o WHERE o.restaurant.id = :restaurantId")
-//	Optional<List<Order>> findOrdersByRestaurantId(@Param("restaurantId") Long restaurantId);
-//}
+package com.soni.repository;
+
+import java.util.List; 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.soni.model.Order;
+import com.soni.model.User;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+	@Query("SELECT o FROM Order o WHERE o.customer.id = :userId")
+	Optional<List<Order>> findAllUserOrders(@Param("userId")Long userId);
+    
+	@Query("SELECT o FROM Order o WHERE o.restaurant.id = :restaurantId")
+	Optional<List<Order>> findOrdersByRestaurantId(@Param("restaurantId") Long restaurantId);
+}
