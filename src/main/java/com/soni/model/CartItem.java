0da4499d1dc789d1edb3f30a3924fb3d312c04b9
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 20:48:15 +0530

    Added CartItem

--- src/main/java/com/soni/model/CartItem.java
+++ src/main/java/com/soni/model/CartItem.java
@@ -1,37 +1,37 @@
-//package com.soni.model;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class CartItem {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @JsonIgnore
-//    @ManyToOne
-//    @JoinColumn(name = "cart_id")
-//    private Cart cart;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "menu_item_id")
-//    private MenuItem menuItem;
-//    
-//    private int quantity;
-//    
-//   
-//}
-//
+package com.soni.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_item_id")
+    private MenuItem menuItem;
+    
+    private int quantity;
+    
+   
+}
+
