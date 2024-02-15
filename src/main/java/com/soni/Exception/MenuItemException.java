0a8e3b28581ec85cde0b8b1acc35986f57fe6d44
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 20:44:19 +0530

    Added MenuItemException

--- src/main/java/com/soni/Exception/MenuItemException.java
+++ src/main/java/com/soni/Exception/MenuItemException.java
@@ -1,10 +1,10 @@
-//package com.soni.Exception;
-//
-//public class MenuItemException extends Exception {
-//
-//	public MenuItemException(String message) {
-//		super(message);
-//
-//	}
-//
-//}
+package com.soni.Exception;
+
+public class MenuItemException extends Exception {
+
+	public MenuItemException(String message) {
+		super(message);
+
+	}
+
+}
