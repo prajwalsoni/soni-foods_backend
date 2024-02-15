926f48e448299c79e992ad0ae81074ea60bf4482
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 20:59:48 +0530

    Added CreateOrderRequest and CreateRestaurantRequest

--- src/main/java/com/soni/request/CreateOrderRequest.java
+++ src/main/java/com/soni/request/CreateOrderRequest.java
@@ -1,15 +1,15 @@
-//package com.soni.request;
-//
-//import com.soni.model.Address; 
-//
-//import lombok.Data;
-//
-//@Data
-//public class CreateOrderRequest {
-// 
-//	private Long restaurantId;
-//	
-//	private Address deliveryAddress;
-//	
-//    
-//}
+package com.soni.request;
+
+import com.soni.model.Address; 
+
+import lombok.Data;
+
+@Data
+public class CreateOrderRequest {
+ 
+	private Long restaurantId;
+	
+	private Address deliveryAddress;
+	
+    
+}
--- src/main/java/com/soni/request/CreateRestaurantRequest.java
+++ src/main/java/com/soni/request/CreateRestaurantRequest.java
@@ -1,25 +1,25 @@
-//package com.soni.request;
-//
-//import java.time.LocalDateTime; 
-//
-//import com.soni.model.Address;
-//import com.soni.model.ContactInformation;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class CreateRestaurantRequest {
-//
-//	private Long id;
-//	private String name;
-//	private String description;
-//	private String cuisineType;
-//	private Address address;
-//	private ContactInformation contactInformation;
-//	private String openingHours;
-//	private String imageUrl;
-//    private LocalDateTime registrationDate;
-//}
+package com.soni.request;
+
+import java.time.LocalDateTime; 
+
+import com.soni.model.Address;
+import com.soni.model.ContactInformation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateRestaurantRequest {
+
+	private Long id;
+	private String name;
+	private String description;
+	private String cuisineType;
+	private Address address;
+	private ContactInformation contactInformation;
+	private String openingHours;
+	private String imageUrl;
+    private LocalDateTime registrationDate;
+}
