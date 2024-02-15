07b0a4873b6bdf11c65d1fb2265769a8f0fa78a3
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 20:48:40 +0530

    Added Category

--- src/main/java/com/soni/model/Category.java
+++ src/main/java/com/soni/model/Category.java
@@ -1,24 +1,24 @@
-//package com.soni.model;
-//
-//import jakarta.persistence.Entity; 
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Category {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    
-//    private String name;
-//    
-// 
-//}
-//
+package com.soni.model;
+
+import jakarta.persistence.Entity; 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    
+ 
+}
+
