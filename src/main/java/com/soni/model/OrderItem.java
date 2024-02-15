b39bcb695e4bbbf8ed4eecdf41b2bca3d42fc782
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 20:50:54 +0530

    Added Order and OrderItem

--- src/main/java/com/soni/model/OrderItem.java
+++ src/main/java/com/soni/model/OrderItem.java
@@ -1,34 +1,34 @@
-//package com.soni.model;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//public class OrderItem {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    
-//    @ManyToOne
-//    private MenuItem menuItem;
-//    
-//    private int quantity;
-//    private Long subtotal;
-//
-//    
-//    
-//}
-//
+package com.soni.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    @ManyToOne
+    private MenuItem menuItem;
+    
+    private int quantity;
+    private Long subtotal;
+
+    
+    
+}
+
