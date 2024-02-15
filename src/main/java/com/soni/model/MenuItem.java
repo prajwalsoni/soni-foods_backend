ce51ac82ae81316bbb0b9cd577d5eb9341202dc8
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 20:50:16 +0530

    Added MEnuItem and Notification

--- src/main/java/com/soni/model/MenuItem.java
+++ src/main/java/com/soni/model/MenuItem.java
@@ -1,56 +1,56 @@
-//package com.soni.model;
-//
-//import java.util.Date;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import jakarta.persistence.OneToMany;
-//import jakarta.persistence.Temporal;
-//import jakarta.persistence.TemporalType;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//public class MenuItem {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    
-//    private String name;
-//    private String description;
-//    private Long price;
-//    
-//    @ManyToOne
-//    private Category category;
-//    private String imageUrl;
-//    private boolean availabilityStatus;
-//
-//    
-//    @ManyToOne
-//    @JoinColumn(name = "restaurant_id")
-//    private Restaurant restaurant;
-//    
-//    private boolean isVegetarian;
-//    private boolean isGlutenFree;
-//    private boolean isVegan;
-//    
-//    private Long discountedPrice;
-//    
-//    private Integer discount;
-//    
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date creationDate;
-//    
-//    }
-//
+package com.soni.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String description;
+    private Long price;
+    
+    @ManyToOne
+    private Category category;
+    private String imageUrl;
+    private boolean availabilityStatus;
+
+    
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+    
+    private boolean isVegetarian;
+    private boolean isGlutenFree;
+    private boolean isVegan;
+    
+    private Long discountedPrice;
+    
+    private Integer discount;
+    
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+    
+    }
+
--- src/main/java/com/soni/model/Notification.java
+++ src/main/java/com/soni/model/Notification.java
@@ -1,35 +1,35 @@
-//package com.soni.model;
-//
-//import java.util.Date;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import jakarta.persistence.Temporal;
-//import jakarta.persistence.TemporalType;
-//import lombok.Data;
-//
-//@Entity
-//@Data
-//public class Notification {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    
-//    @ManyToOne
-//    @JoinColumn(name = "recipient_id")
-//    private User recipient; // This could be User, Restaurant, or DeliveryExecutive
-//    
-//    private String message;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date sentAt;
-//    
-//    private boolean readStatus;
-//    
-//    
-//}
-//
+package com.soni.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Data
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "recipient_id")
+    private User recipient; // This could be User, Restaurant, or DeliveryExecutive
+    
+    private String message;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date sentAt;
+    
+    private boolean readStatus;
+    
+    
+}
+
