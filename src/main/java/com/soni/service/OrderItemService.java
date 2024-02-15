a82591ea216c2e711d47d1e8133be7aa462dbbb8
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 21:05:39 +0530

    Added Notification Service and OrderItemService

--- src/main/java/com/soni/service/OrderItemService.java
+++ src/main/java/com/soni/service/OrderItemService.java
@@ -1,9 +1,9 @@
-//package com.soni.service;
-//
-//import com.soni.model.OrderItem;
-//
-//public interface OrderItemService {
-//	
-//	public OrderItem createOrderIem (OrderItem orderItem);
-//
-//}
+package com.soni.service;
+
+import com.soni.model.OrderItem;
+
+public interface OrderItemService {
+	
+	public OrderItem createOrderIem (OrderItem orderItem);
+
+}
