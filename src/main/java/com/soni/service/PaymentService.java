8ec9e7a8b6f8355c583ce82213e8ed309f6490ba
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 21:08:27 +0530

    Added PasswordResetTokenServiceImplementation , PaymentService and PaymentService Implementation

--- src/main/java/com/soni/service/PaymentService.java
+++ src/main/java/com/soni/service/PaymentService.java
@@ -1,12 +1,12 @@
-//package com.soni.service;
-//
-//import com.stripe.exception.StripeException;
-//
-//import com.soni.model.Order;
-//import com.soni.model.PaymentResponse;
-//
-//public interface PaymentService {
-//	
-//	public PaymentResponse generatePaymentLink(Order order) throws StripeException;
-//
-//}
+package com.soni.service;
+
+import com.stripe.exception.StripeException;
+
+import com.soni.model.Order;
+import com.soni.model.PaymentResponse;
+
+public interface PaymentService {
+	
+	public PaymentResponse generatePaymentLink(Order order) throws StripeException;
+
+}
