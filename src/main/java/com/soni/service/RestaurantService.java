630fbb9bfe5c4c096ae0b7b1d282a57c3d3d82ed
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 21:09:30 +0530

    Added RestaurantService,RestaurantServiceImplementation and ReviewSerive

--- src/main/java/com/soni/service/RestaurantService.java
+++ src/main/java/com/soni/service/RestaurantService.java
@@ -1,32 +1,32 @@
-//package com.soni.service;
-//
-//import java.util.List; 
-//
-//import com.soni.Exception.RestaurantException;
-//import com.soni.dto.RestaurantDto;
-//import com.soni.model.Restaurant;
-//import com.soni.model.User;
-//import com.soni.request.CreateRestaurantRequest;
-//
-//public interface RestaurantService {
-//
-//	public Restaurant createRestaurant(CreateRestaurantRequest req,User user);
-//
-//	public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updatedRestaurant)
-//			throws RestaurantException;
-//
-//	public void deleteRestaurant(Long restaurantId) throws RestaurantException;
-//
-//	public Restaurant findRestaurantByName(String Name) throws RestaurantException;
-//
-//	public List<Restaurant> getRestaurantsByName(String name);
-//	
-//	public List<Restaurant>getAllRestaurant();
-//	
-//	public Restaurant findRestaurantById(Long id) throws RestaurantException;
-////	 public  List<Restaurant> getRestaurantsByLocation(String location);
-//	public List<Restaurant> getRestaurantsByUserId(Long userId) throws RestaurantException;
-//	
-//	public RestaurantDto addToFavorites(Long restaurantId,User user) throws RestaurantException;
-//
-//}
+package com.soni.service;
+
+import java.util.List; 
+
+import com.soni.Exception.RestaurantException;
+import com.soni.dto.RestaurantDto;
+import com.soni.model.Restaurant;
+import com.soni.model.User;
+import com.soni.request.CreateRestaurantRequest;
+
+public interface RestaurantService {
+
+	public Restaurant createRestaurant(CreateRestaurantRequest req,User user);
+
+	public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updatedRestaurant)
+			throws RestaurantException;
+
+	public void deleteRestaurant(Long restaurantId) throws RestaurantException;
+
+	public Restaurant findRestaurantByName(String Name) throws RestaurantException;
+
+	public List<Restaurant> getRestaurantsByName(String name);
+	
+	public List<Restaurant>getAllRestaurant();
+	
+	public Restaurant findRestaurantById(Long id) throws RestaurantException;
+//	 public  List<Restaurant> getRestaurantsByLocation(String location);
+	public List<Restaurant> getRestaurantsByUserId(Long userId) throws RestaurantException;
+	
+	public RestaurantDto addToFavorites(Long restaurantId,User user) throws RestaurantException;
+
+}
--- src/main/java/com/soni/service/ReviewSerive.java
+++ src/main/java/com/soni/service/ReviewSerive.java
@@ -1,17 +1,17 @@
-//package com.soni.service;
-//
-//import java.util.List; 
-//
-//import com.soni.Exception.ReviewException;
-//import com.soni.model.Review;
-//import com.soni.model.User;
-//import com.soni.request.ReviewRequest;
-//
-//public interface ReviewSerive {
-//	
-//    public Review submitReview(ReviewRequest review,User user);
-//    public void deleteReview(Long reviewId) throws ReviewException;
-//    public double calculateAverageRating(List<Review> reviews);
-//	public Review updateReview(Long reviewId, ReviewRequest updatedReview) throws ReviewException;
-//
-//}
+package com.soni.service;
+
+import java.util.List; 
+
+import com.soni.Exception.ReviewException;
+import com.soni.model.Review;
+import com.soni.model.User;
+import com.soni.request.ReviewRequest;
+
+public interface ReviewSerive {
+	
+    public Review submitReview(ReviewRequest review,User user);
+    public void deleteReview(Long reviewId) throws ReviewException;
+    public double calculateAverageRating(List<Review> reviews);
+	public Review updateReview(Long reviewId, ReviewRequest updatedReview) throws ReviewException;
+
+}
